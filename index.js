d11e86656fb0d39ac4aa138ce6b9200a0d5d2f94
Jose G Martinez <jgermanmm@gmail.com>
2025-12-22 21:04:16 -0300

    add index.html, index.js, and live-demo1.js files

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// Este es un comentario
+
+/*
+Comentarios de mas de una
+linea 
+sdfg
+dfg
+dfg
+dfg
+df
+gdf
+g
+*/
+
+let x = 5 // declaración y asignación
+let y = 10 // declaración y asignación
+let suma = x + y // declaración y asignación
+console.log(suma)  // es una función incorporada dentro del lenguaje
+
+
+let nombreCompleto  //declaración de una variable
+const edad = 25  // se debe asignar por obligación del lenguaje un valor al momento de declarar con const
+
+nombreCompleto = "Alberto Diaz" // asignación de un valor a una variable (   =    )
+
+//alert("Mensaje desde JavaScript")
+
+//confirm("Seguro eliminas el archivo?")
+
+//prompt("Ingresa tu nombre: ")
+
+// console.log("Mensaje después del Alert!!!")
+
+let nombre = "José"
+let apellido = "Martínez"
+
+//console.log(nombre + " " + apellido)  //concatenar  -->> JoséMartinez
+
+//console.log(`${nombre} ${apellido}`)
---
+++ live-demo1.js
@@ -0,0 +1,12 @@
+
+
+
+// solicitar al usuario nombre y edad
+
+let nombre = prompt("Ingrese tu nombre: ")
+let edad = prompt("Ingrese su edad: ")
+
+
+// Imprimir un mensaje de bienvenida en la consola
+
+console.log(`Hola, ${nombre}. Te damos la bienvenida a nuestra aplicación. Ya sabemos que tienes ${edad} años y que te sientes muy jovial.`)
